a17d58a25816b724286d7b4ca05becd2e10326a6
veiset <veiset@gmail.com>
2012-10-16 22:26:05 +0200

    Added support for tool support for animations. Fun stuff.

--- tools/modelmaker/main.py
+++ tools/modelmaker/main.py
@@ -7,24 +7,22 @@ import colset
 fpsClock = pygame.time.Clock()
 
 pygame.init()
-
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Main')
 
+playButton = pygame.image.load("img/play.png").convert()
 
 game = True
-rgb = [0, 0, 0]
-multi = False
+activeTab = 0
+# Generating colorset
 colors = []
-c = 0
 l = []
-for x in colset.rgb:
+for i, x in enumerate(colset.rgb):
     l.append(x)     
-    c += 1
-    if (c==16):
-        c = 0
+    if ((i+1)%16==0): # display 16 colors per row
         colors.append(l)
         l = []
+colors.append(l)
 
 lastUsed = []
 
@@ -43,7 +41,6 @@ def outputModel():
 
     print "    <ENTITY>"
     print "        <NAME>EditMe</NAME>"
-    print "        <LEVEL>front</LEVEL>"
     print "        <MODEL>"
     print "[",
     for y in range(my):
@@ -55,13 +52,45 @@ def outputModel():
     print "        </MODEL>"
     print "    </ENTITY>"
 
+def gridEmpty(model):
+    for row in model:
+        for col in row:
+            if col!=0: return False
+    return True
 
+def areaColorGrid(mx,my): 
+    return (mx < 200 and my < 500)
 
-colors.append(l)
+def areaHistoryTab(mx,my):
+    print (mx, my)
+    return (mx < 200 and 520>my>500)
+
+def areaClearBoardButton(mx,my):
+    return (mx < 30 and 600>my>570)
+
+def areaModel(mx,my):
+    return (mx>200 and my>30)
+
+def areaModelTab(mx,my):
+    return (mx>200 and my<30)
+
+def areaPlayButton(mx,my):
+    return (mx>200 and ((mx-200)/30 == 19) and my<30)
+
+def areaSpeedButton(mx,my):
+    return (mx>200 and ((mx-200)/30 == 18) and my<30)
+
 pygame.mouse.set_visible(1)
 
 currentCol = (255,0,0)
-model = [[0 for _ in range(20)] for _ in range(20)]
+def emptyModel():
+    return [[0 for _ in range(20)] for _ in range(20)]
+
+modelGrid = [emptyModel() for _ in range(20)]
+model = modelGrid[0]
+playAnim = False
+speed = 0
+speedTicks = [1,3,6,10,30]
 while game:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -70,25 +99,51 @@ while game:
         
         if event.type == MOUSEBUTTONDOWN:
             mx, my = event.pos
-            if mx < 200 and my<500:
+            mxGrid = (mx-200)/30
+            myGrid = my/30
+
+            # Change color
+            if areaColorGrid(mx,my):
                 try:
                     currentCol = colors[my/12][mx/12]
                     lastUsed.append(currentCol)
                 except:
                     print 'out of bound'
-            elif mx < 200 and 520>my>500:
+            elif areaHistoryTab(mx,my):
                 try:
                     currentCol = lastUsed[::-1][mx/20]
                 except:
                     print 'out of bound'
-            elif mx < 30 and 600>my>570:
-                model = [[0 for _ in range(20)] for _ in range(20)]
-            elif mx>200:
-                index = ((mx-200)/30, (my)/30)
+            elif areaClearBoardButton(mx,my):
+                modelGrid[activeTab] = emptyModel()
+                model = modelGrid[activeTab]
+            elif areaModel(mx,my):
+                index = (mxGrid, myGrid)
                 if event.button == 1:
                     model[index[0]][index[1]] = currentCol
                 else:
                     model[index[0]][index[1]] = 0
+            elif areaPlayButton(mx,my):
+                playAnim = not playAnim
+                if playAnim:
+                    activeTab = 0
+            elif areaSpeedButton(mx,my):
+                if (speed+1) == len(speedTicks): speed = 0
+                else: speed += 1
+                print("speed =", speed)
+            elif areaModelTab(mx,my):
+                # Copy model from prev grid
+                if (activeTab == mxGrid) and 19>mxGrid>0:
+                    # make a copy
+                    copyTab = modelGrid[activeTab-1]
+                    copy = []
+                    for row in copyTab:
+                        copy.append(list(row))
+                    modelGrid[activeTab] = copy
+
+                print("tab=", mxGrid)
+                activeTab = mxGrid
+                model = modelGrid[mxGrid]
 
         if event.type == KEYDOWN:
             outputModel()
@@ -119,7 +174,24 @@ while game:
 
     pygame.draw.rect(screen, (0,0,0), (0, 570, 30, 30))
 
-    pygame.display.update()
-    fpsClock.tick(60)
+    # Model tabs
+    for x in range(20):
+        pygame.draw.rect(screen, (255-(x*10),)*3, (200+(30*x),0,30,30))
+    screen.blit(playButton, (800-30, 0))
+    # Highlighted tab
+    pygame.draw.rect(screen, (242,150,150), (200+(30*activeTab),0,30,30))
+    # Speed Button 
+    pygame.draw.rect(screen, (0,150,0), (800-(30*2),0,30,30))
     
 
+    pygame.display.update()
+
+    if playAnim:
+        if gridEmpty(modelGrid[activeTab+1]):
+            activeTab = 0
+        else:
+            activeTab += 1
+        model = modelGrid[activeTab]
+        fpsClock.tick(speedTicks[speed])
+    else:
+        fpsClock.tick(60)
